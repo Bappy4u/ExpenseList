4a5ee44f9a4c54e86f8f921bd233d33a86902665
Bappy <http.bappy@gmail.com>
2024-03-20 03:50:20 +0600

    Initializeing form

---
+++ src/components/Form.tsx
@@ -0,0 +1,16 @@
+const Form = () => {
+    return (
+        <>
+            <form action="">
+                <label htmlFor="">Description</label>
+                <input type="text"/>
+                <label htmlFor="">Name</label>
+                <input type="text"/>
+                
+            </form>
+        </>
+
+    )
+};
+
+export default Form;
